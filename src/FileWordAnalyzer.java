034b76d6fddef79f0c3fd397aa2f1820adeaa78f
polon59 <przemyslaw.polczak@gmail.com>
2018-09-10 02:07:40 +0200

    Add method 'wordsByABC' in class 'FileWordAnalyzer,java'

--- src/FileWordAnalyzer.java
+++ src/FileWordAnalyzer.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWordAnalyzer {
+
+    FilePartReader filePartReader;
+
+    public FileWordAnalyzer(FilePartReader filePartReader){
+        this.filePartReader = filePartReader;
+    }
+
+    public ArrayList wordsByABC(){
+        String fileText = filePartReader.readLines();
+        String[] fileLines = fileText.split("\n");
+        Arrays.sort(fileLines);
+
+
+        return new ArrayList<String>(Arrays.asList(fileLines));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         FilePartReader filePartReader = new FilePartReader();
+        FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer(filePartReader);
+
         filePartReader.setup("data.txt",1,6);
         System.out.println(filePartReader.readLines());
+
+        List<String> wordsAlphabhetically = fileWordAnalyzer.wordsByABC();
+        for (String word : wordsAlphabhetically){
+            System.out.println(word);
+        }
     }
 }
