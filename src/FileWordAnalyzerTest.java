74155891a6546faf9da5294e156ce90948b240ba
polon59 <przemyslaw.polczak@gmail.com>
2018-09-10 02:54:23 +0200

    Create test for method 'wordsByABC'

--- src/FileWordAnalyzerTest.java
+++ src/FileWordAnalyzerTest.java
@@ -9,7 +9,15 @@ class FileWordAnalyzerTest {
     void wordsByABCTest() {
         FilePartReader filePartReader = new FilePartReader();
         FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer(filePartReader);
+        filePartReader.setup("data.txt",1,3);
+
         List<String> wordsAlphabhetically = fileWordAnalyzer.wordsByABC();
+
         List<String> expectedWords = new ArrayList<>();
-        expectedWords.add("dupa"); }
+        expectedWords.add("dupa");
+        expectedWords.add("jakieś słowo");
+        expectedWords.add("line2");
+
+        assertEquals(expectedWords, wordsAlphabhetically);
+    }
 }
